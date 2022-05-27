54b92cbb70bc44169c774a3fcd28d6df8a3f83ea
pradeep kadati <pradeepkadati@gmail.com>
2022-05-27 22:06:37 +0530

    Initial Setup

---
+++ Streams/java-streams/src/test/java/io/kp/crazybee/streams/AppTest.java
@@ -0,0 +1,22 @@
+package io.kp.crazybee.streams;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+
+import org.junit.jupiter.api.Test;p.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
